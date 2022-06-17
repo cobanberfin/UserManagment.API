2f638bf6722c098144c8fb2748ca954257b923aa
berfin çoban <berfincoban954@gmail.com>
2022-06-17 12:18:56 +0300

    code

--- UserManagment.API/Controllers/UsersController.cs
+++ UserManagment.API/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ namespace UserManagment.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private List<User> _users = Fakedata.GetUsers(200);
         [HttpGet]
@@ -19,8 +19,8 @@ namespace UserManagment.API.Controllers
         [HttpGet("{id}")]
         public User Get(int id)
         {
-            var user =_users.FirstOrDefault(x=>x.Id== id);
-            return user;  
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return user;
 
         }
         //gonderdıgın requestın bodysınde user nesnesı beklıyorum gonder bnede bunu lısetye eklıyım
@@ -30,17 +30,17 @@ namespace UserManagment.API.Controllers
             _users.Add(user);
             return user;
         }
-        [HttpPost]
-        public  User Put([FromBody] User user)
+        [HttpPut]
+        public User Put([FromBody] User user)
         {
-            var editedUser = _users.FirstOrDefault(x=>x.Id== user.Id);
+            var editedUser = _users.FirstOrDefault(x => x.Id == user.Id);
             editedUser.Firstname = user.Firstname;
             editedUser.Lastname = user.Lastname;
             editedUser.Address = user.Address;
             _users.Add(user);
             return user;
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public void Delete (int id)
         {
             var deleteUser = _users.FirstOrDefault(x => x.Id == id);
